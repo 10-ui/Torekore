c24b869824baddcca92e3a8c5ff2fa032a56d0d9
10-ui <ui-10@outlook.jp>
2024-06-03 14:47:40 +0900

    nativecn-uiを参考に下記2点を追加 ・input.tsx ・button.tsx

--- components/input.tsx
+++ components/input.tsx
@@ -1,2 +1,39 @@
 import React from 'react';
-import { TextInput } from 'react-native';
+import { docking } from '@/utils/docking';
+import { Text, TextInput, View } from 'react-native';
+
+export interface InputProps
+  extends React.ComponentPropsWithoutRef<typeof TextInput> {
+  label?: string;
+  labelClasses?: string;
+  inputClasses?: string;
+}
+const Input = React.forwardRef<
+  React.ElementRef<typeof TextInput>,
+  InputProps
+>(
+  (
+    { className, label, labelClasses, inputClasses, ...props },
+    ref
+  ) => (
+    <View className={docking('flex flex-col gap-1.5', className)}>
+      {label && (
+        <Text className={docking('text-base', labelClasses)}>
+          {label}
+        </Text>
+      )}
+      <TextInput
+        ref={ref}
+        className={docking(
+          inputClasses,
+          'border border-input py-2.5 px-4 rounded-lg'
+        )}
+        {...props}
+      />
+    </View>
+  )
+);
+
+Input.displayName = 'Input';
+
+export { Input };
