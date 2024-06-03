0902b7b912a9de778f3f1367573f8452292ff869
10-ui <ui-10@outlook.jp>
2024-06-03 13:59:23 +0900

    buttonコンポーネントの基本実装

--- components/button.tsx
+++ components/button.tsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import { Button } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+  onPress: () => void;
+  disabled: boolean;
+}
+
+const AppButton = ({ title, onPress, disabled }: ButtonProps) => {
+  return (
+    <Button title={title} onPress={onPress} disabled={disabled} />
+  );
+};
+
+export default AppButton;
