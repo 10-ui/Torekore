8c9a416b85582aacc9a0a000c50e4978ea15d2fa
hirase maru <23aw0136@jec.ac.jp>
2024-06-04 10:02:03 +0900

    仙太郎

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import '@/styles/global.css';
+import { Button } from '@/components/button';
 
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button label="Button" onPress={() => console.log('Button pressed')} />
+      <Button label="Button" onPress={() => console.log('Button pressed')} />
+      <Image source={require('@/assets/googleLogo.svg')} />
+      <Text>Googleでログイン</Text>
+      <Image source={require('@/assets/XLogo.svg')} />
+      <Text>Xでログイン</Text>
       <StatusBar style='auto' />
     </View>
   );
