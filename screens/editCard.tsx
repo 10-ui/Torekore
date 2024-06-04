175d00bf9f2a878cc2c8684b9049b8ffac5f841e
10-ui <ui-10@outlook.jp>
2024-06-04 11:50:39 +0900

    tabを導入。

--- screens/editCard.tsx
+++ screens/editCard.tsx
@@ -1,8 +1,35 @@
 import { View, Text } from 'react-native';
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from '@/components/tabs';
 
 export default function EditCard() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
+      <Tabs defaultValue='account'>
+        <TabsList>
+          <TabsTrigger id='account' title='Account' value='account' />
+          <TabsTrigger
+            id='password'
+            title='Password'
+            value='password'
+          />
+        </TabsList>
+        <TabsContent value='account'>
+          <Text className='text-primary'>
+            Make changes to your account here.
+          </Text>
+        </TabsContent>
+        <TabsContent value='password'>
+          <Text className='text-primary'>
+            Change your password here.
+          </Text>
+        </TabsContent>
+      </Tabs>
+
       <Text>EditCard</Text>
     </View>
   );
