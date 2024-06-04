8eb74365ac7bedac71632954cfc4775521a2d717
10-ui <ui-10@outlook.jp>
2024-06-04 10:11:10 +0900

    ロゴ移動に伴うAppの変更

--- App.tsx
+++ App.tsx
@@ -6,11 +6,17 @@ import { Button } from '@/components/button';
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Button label="Button" onPress={() => console.log('Button pressed')} />
-      <Button label="Button" onPress={() => console.log('Button pressed')} />
-      <Image source={require('@/assets/googleLogo.svg')} />
+      <Button
+        label='Button'
+        onPress={() => console.log('Button pressed')}
+      />
+      <Button
+        label='Button'
+        onPress={() => console.log('Button pressed')}
+      />
+      <Image source={require('@/assets/logos/google.svg')} />
       <Text>Googleでログイン</Text>
-      <Image source={require('@/assets/XLogo.svg')} />
+      <Image source={require('@/assets/logos/X.svg')} />
       <Text>Xでログイン</Text>
       <StatusBar style='auto' />
     </View>
