87c94c70c7d2fc49a03de34b52ff0d6a8142d83c
10-ui <ui-10@outlook.jp>
2024-06-11 15:28:22 +0900

    整形ファイル

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,8 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
-  input: './styles/global.css',
+  input: "./styles/global.css",
 });
