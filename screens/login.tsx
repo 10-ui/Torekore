6c6b02dec73262af4a98d6e55319d9b4acccee8f
teruhasi <23aw0136@jec.ac.jp>
2024-06-11 16:59:05 +0900

    ログインとサインアップのコンポーネントを仮修正　

--- screens/login.tsx
+++ screens/login.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/button';
 import { Image } from 'expo-image';
 import { Text, View } from 'react-native';
+import { Input } from '@/components/input';
 
 export default function Login({
   setAuthentication,
@@ -9,10 +10,23 @@ export default function Login({
 }) {
   return (
     <View className='flex-1 items-center justify-center bg-white px-13'>
+      <Image
+        source={require('@/assets/logos/applogo.svg')}
+        style={{ width: 100, height: 100 }}
+      />
+      <Input
+        label='メールアドレス'
+        placeholder='メールアドレスを入力してください'  
+      />
+      <Input
+        label='パスワード'
+        placeholder='パスワードを入力してください'  
+      />
       <Button
         label='ログイン'
         onPress={() => setAuthentication(true)}
       />
+      
       <Button label='初めて利用される方はこちら' variant='outline' />
       <Image
         source={require('@/assets/logos/google.svg')}
@@ -23,7 +37,7 @@ export default function Login({
         source={require('@/assets/logos/X.svg')}
         style={{ width: 50, height: 50 }}
       />
-      <Text>Xで連携</Text>
+      <Text>イーロンマスクと恋愛</Text>
     </View>
   );
 }
--- screens/signup.tsx
+++ screens/signup.tsx
@@ -1,9 +1,27 @@
 import { View, Text } from 'react-native';
+import { Input } from '@/components/input';
+import { Button } from '@/components/button';
+import { Image } from 'expo-image';
 
-export const Signup = () => {
+
+export default function Signup() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text>Signup</Text>
-    </View>
+    <View className='flex-1 items-center justify-center bg-white px-13'>
+    <Button
+      label='ログイン'
+      
+    />
+    <Button label='初めて利用される方はこちら' variant='outline' />
+    <Image
+      source={require('@/assets/logos/google.svg')}
+      style={{ width: 50, height: 50 }}
+    />
+    <Text>Googleで連携</Text>
+    <Image
+      source={require('@/assets/logos/X.svg')}
+      style={{ width: 50, height: 50 }}
+    />  
+    <Text>Xで連携</Text>
+  </View>
   );
 };
