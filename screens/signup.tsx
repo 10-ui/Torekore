6ec1d50639830d726b2174068f98de8835f331f7
10-ui <ui-10@outlook.jp>
2024-06-11 13:35:39 +0900

    切り分けに伴う位置変更

--- screens/signup.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export const Signup = () => {
-  return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text>Signup</Text>
-    </View>
-  );
-};
