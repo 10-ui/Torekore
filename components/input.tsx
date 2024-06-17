9b7ae343180969ba77e1e30105fbbf0a9e5dbf60
10-ui <ui-10@outlook.jp>
2024-06-17 20:03:22 +0900

    コンポーネントの修正

--- components/input.tsx
+++ components/input.tsx
@@ -6,9 +6,10 @@ export interface InputProps
   extends React.ComponentPropsWithoutRef<typeof TextInput> {
   label?: string;
   labelClasses?: string;
+  rightIcon?: React.ReactNode;
 }
 const Input = React.forwardRef<React.ElementRef<typeof TextInput>, InputProps>(
-  ({ className, label, labelClasses, ...props }, ref) => (
+  ({ className, label, labelClasses, rightIcon, ...props }, ref) => (
     <View className={docking("flex w-full flex-col gap-1", className)}>
       {label && (
         <Text className={docking("text-base", labelClasses)}>{label}</Text>
@@ -16,11 +17,16 @@ const Input = React.forwardRef<React.ElementRef<typeof TextInput>, InputProps>(
       <TextInput
         ref={ref}
         className={docking(
-          "flex h-12 w-full items-center justify-center rounded-lg border border-input px-3 text-slate-400",
+          "relative flex h-12 w-full items-center justify-center rounded-lg border border-input p-3 text-slate-400",
           className,
         )}
         {...props}
       />
+      {rightIcon && (
+        <View className='absolute right-3 top-[calc(50%+2px)] h-6 w-6 items-center justify-center p-1'>
+          {rightIcon}
+        </View>
+      )}
     </View>
   ),
 );
