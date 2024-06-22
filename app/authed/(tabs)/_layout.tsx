6dc9cd07f14348d75e2945511369ad8c5f404ebe
10-ui <ui-10@outlook.jp>
2024-06-22 13:18:42 +0900

    ActionSheetの追加。UI基盤完成なのでしっかりしたUIと、backend側の設定、signup,loginの設定まち！！！！

--- app/authed/(tabs)/_layout.tsx
+++ app/authed/(tabs)/_layout.tsx
@@ -1,72 +1,75 @@
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { Ionicons, MaterialCommunityIcons, Octicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { Tabs } from "expo-router";
 
 export default function AuthedLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name='index'
-        options={{
-          headerStyle: {
-            height: 100,
-          },
-          headerTitle: () => {
-            return (
-              <Image
-                source={require("@/assets/logos/applogo.svg")}
-                style={{ width: 100, height: 30 }}
+    <ActionSheetProvider>
+      <Tabs>
+        <Tabs.Screen
+          name='index'
+          options={{
+            headerStyle: {
+              height: 100,
+            },
+            headerTitle: () => {
+              return (
+                <Image
+                  source={require("@/assets/logos/applogo.svg")}
+                  style={{ width: 100, height: 30 }}
+                />
+              );
+            },
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name='home'
+                size={24}
+                color={focused ? "dodgerblue" : "gray"}
               />
-            );
-          },
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name='home'
-              size={24}
-              color={focused ? "dodgerblue" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='editCard'
-        options={{
-          title: "カード編集",
-          tabBarIcon: ({ focused }) => (
-            <MaterialCommunityIcons
-              name='card-account-details'
-              size={24}
-              color={focused ? "dodgerblue" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='shareCard'
-        options={{
-          title: "カード交換",
-          tabBarIcon: ({ focused }) => (
-            <Octicons
-              name='share'
-              size={24}
-              color={focused ? "dodgerblue" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='allCards'
-        options={{
-          title: "カード一覧",
-          tabBarIcon: ({ focused }) => (
-            <MaterialCommunityIcons
-              name='cards'
-              size={24}
-              color={focused ? "dodgerblue" : "gray"}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='editCard'
+          options={{
+            title: "カード編集",
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons
+                name='card-account-details'
+                size={24}
+                color={focused ? "dodgerblue" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='shareCard'
+          options={{
+            title: "カード交換",
+            tabBarIcon: ({ focused }) => (
+              <Octicons
+                name='share'
+                size={24}
+                color={focused ? "dodgerblue" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='allCards'
+          options={{
+            title: "カード一覧",
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons
+                name='cards'
+                size={24}
+                color={focused ? "dodgerblue" : "gray"}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </ActionSheetProvider>
   );
 }
--- app/authed/(tabs)/allCards.tsx
+++ app/authed/(tabs)/allCards.tsx
@@ -1,10 +1,51 @@
 import { Button } from "@/components/button";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { useActionSheet } from "@expo/react-native-action-sheet";
+import { useState } from "react";
 
 export default function AllCards() {
+  const { showActionSheetWithOptions } = useActionSheet();
+  const [label, setLabel] = useState("レベルが高い順");
+  const handleSort = () => {
+    const options = ["レベルが高い順", "古い順", "新しい順", "キャンセル"];
+    const cancelButtonIndex = 3;
+
+    showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      (selectedIndex?: number) => {
+        switch (selectedIndex) {
+          case 0:
+            // レベルが高い順
+            setLabel("レベルが高い順");
+            break;
+
+          case 1:
+            // 古い順
+            setLabel("古い順");
+            break;
+
+          case 2:
+            // 新しい順
+            setLabel("新しい順");
+            break;
+
+          case cancelButtonIndex:
+          // Canceled
+        }
+      },
+    );
+  };
   return (
-    <View className='flex-1 items-center p-4 bg-white'>
-      <Button variant='outline' className=" w-40 h-10 ml-auto" label='レベルが高い順' />
+    <View className='flex-1 items-center bg-white p-4'>
+      <Button
+        variant='outline'
+        className='ml-auto h-10 w-40'
+        label={label}
+        onPress={handleSort}
+      />
     </View>
   );
 }
