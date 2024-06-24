4ec11fb0a5b64d5557371a19ee681b23bc68583d
10-ui <ui-10@outlook.jp>
2024-06-24 16:06:42 +0900

    input修正完了、bunの再インストール

--- components/input.tsx
+++ components/input.tsx
@@ -1,39 +1,45 @@
 import React from "react";
 import { docking } from "@/utils/docking";
 import { Text, TextInput, View } from "react-native";
+import { Image } from "expo-image";
 
 export interface InputProps
   extends React.ComponentPropsWithoutRef<typeof TextInput> {
   label?: string;
   labelClasses?: string;
+  source?: string;
 }
+
 const Input = React.forwardRef<React.ElementRef<typeof TextInput>, InputProps>(
-  ({ className, label, labelClasses, ...props }, ref) => (
+  ({ className, label, labelClasses, source, ...props }, ref) => (
     <>
-      {label ? (
-        <View className={docking("flex w-full flex-col gap-1", className)}>
+      <View className={docking("flex w-full flex-col gap-1", className)}>
+        {label && (
           <Text className={docking("text-base", { className: labelClasses })}>
             {label}
           </Text>
-          <TextInput
-            ref={ref}
-            className={docking(
-              className,
-              "flex h-12 w-full items-center justify-center rounded-lg border border-input p-3 text-slate-400",
-            )}
-            {...props}
-          />
-        </View>
-      ) : (
+        )}
         <TextInput
           ref={ref}
           className={docking(
             className,
-            "flex h-12 w-full items-center justify-center rounded-lg border border-input p-3 text-slate-400",
+            "flex h-12 w-full items-center justify-center rounded-lg border border-input bg-appLightBlue p-3 text-slate-400",
           )}
           {...props}
         />
-      )}
+        {source && (
+          <Image
+            source={source}
+            style={{
+              width: 20,
+              height: 20,
+              position: "absolute",
+              right: 16,
+              bottom: 12,
+            }}
+          />
+        )}
+      </View>
     </>
   ),
 );
