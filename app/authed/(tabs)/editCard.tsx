79c875748fe5355c1c11ccdf9ddc0a61a45e5397
teruhasi <23aw0136@jec.ac.jp>
2024-07-09 12:17:59 +0900

    カードスタイル変更

--- app/authed/(tabs)/editCard.tsx
+++ app/authed/(tabs)/editCard.tsx
@@ -1,6 +1,9 @@
 import { Text, View, ScrollView } from "react-native";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/tabs";
 import CardInfo from "@/components/card/card-info";
+import CardView from "@/components/card/card-view";
+import ExpoImage from "@/components/expo-image";
+import { Button } from "@/components/button";
 
 export default function EditCard() {
   return (
@@ -26,8 +29,28 @@ export default function EditCard() {
           </TabsContent>
           <TabsContent value='カードスタイル'>
             <View className='bg-missionBlue h-60 w-full space-y-7'>
-              <Text>背景</Text>
-              <Text>フォント</Text>
+              <CardView />
+              <View className='h-30 border border-input px-3 py-4'>
+                <Text className='mb-4'>背景</Text>
+                <View className='flex flex-row gap-2'>
+                  <ExpoImage
+                    source={require("@/assets/background/bg_blue.png")}
+                    className='h-14 w-23 border border-input'
+                  />
+                  <ExpoImage
+                    source={require("@/assets/background/bg_red.png")}
+                    className='h-14 w-23 border border-input'
+                  />
+                  <ExpoImage
+                    source={require("@/assets/background/bg_yellow.png")}
+                    className='h-14 w-23 border border-input'
+                  />
+                </View>
+              </View>
+              <View className='h-12 mt-2 border border-input'>
+                <Text>フォント</Text>
+              </View>
+              <Button label='保存してプレビュー' className='mt-6' />
             </View>
           </TabsContent>
         </Tabs>
